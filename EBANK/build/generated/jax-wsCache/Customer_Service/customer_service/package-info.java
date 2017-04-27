bdac1fad8b7b835982bef115d352e5a567da6a2a
Fathima Sharfa Nasser <sharfa1002000@gmail.com>
2017-04-28 04:12:46 +0530

    Add files via upload

---
+++ EBANK/build/generated/jax-wsCache/Customer_Service/customer_service/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://customer_service/")
+package customer_service;
---
+++ EBANK/build/generated/jax-wsCache/Employee_Service/employee_webservice/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://employee_webservice/")
+package employee_webservice;
